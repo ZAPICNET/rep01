e6d6002a16f7fcc55476f81ddd27acbd1d5e2f09
ZAPICNET <net.zapic@gmail.com>
2021-02-07 23:01:07 +0100

    Create auto-updating app 2

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const console = require('console');
 const { app, BrowserWindow, ipcMain } = require('electron');
 const { autoUpdater } = require('electron-updater');
 
@@ -42,11 +43,19 @@ ipcMain.on('app_version', (event) => {
   event.sender.send('app_version', { version: app.getVersion() });
 });
 
+var front;
+
+ipcMain.on('consoleLog', (event) => {
+  front = event.sender;
+});
+
 autoUpdater.on('update-available', () => {
+  front.send('consoleLog', { text: "Update available!" });
     mainWindow.webContents.send('update_available');
 });
 
 autoUpdater.on('update-downloaded', () => {
+    front.send('consoleLog', { text: "Update downloaded!" });
     mainWindow.webContents.send('update_downloaded');
 });
 
